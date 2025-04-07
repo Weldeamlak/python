f013e17f33efd05cfc697d3da5a003bd77b7b93b
Weldeamlak Mezgebu <weldeamlak12@gmail.com>
2025-04-07 22:28:17 +0300

    first python commit

---
+++ variables.py
@@ -0,0 +1,30 @@
+# python variables
+
+message ="hellow phyton crash course!"
+print(message)
+
+# reasigning a variable
+message = "phthon is an amazing language dueto it's simplicity"
+print(message)
+
+# embeding using a variable
+first_name = "jHON   "
+last_name = "danEl"
+full_name = f"{first_name}{last_name}"
+# Remove  extra whitespace from the full name
+full_name = full_name.strip()
+print(f"Hello {full_name.title()}!")
+
+url = "https://example.com/page"
+# Remove the 'https://' prefix from the URL
+cleaned_url = url.removeprefix("https://")
+
+print(cleaned_url)
+
+# combine of tabs and new lines
+print("deps:\n\tSoftwar Engineer\n\tComputer Science\n\t& \n\tInformation System")
+
+famous_quote = 'Albert Einstein once said, "A person who never made a mistake never tried anything new".'
+print(famous_quote)
+
+# class end 🏆🏆 for today april 7 / 2025;
---
+++ welcome.py
@@ -0,0 +1,2 @@
+# frist code in python
+print("print is one type of built-in fuction in python library ")
